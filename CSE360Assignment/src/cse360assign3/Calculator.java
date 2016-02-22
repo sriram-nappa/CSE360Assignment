9bcaba564dffe8252eb3f14eeffae1904e34ea96
Sriram <2k6sriram@gmail.com>
2016-02-22 15:10:38 -0700

    Assignment 3 - Initial Version

--- CSE360Assignment/src/cse360assign3/Calculator.java
+++ CSE360Assignment/src/cse360assign3/Calculator.java
@@ -1,11 +1,10 @@
 package cse360assign3;
 
 /**
- * Final Version of calculator class contains seven stub methods including
+ * Initial Version of calculator class contains seven stub methods including
  * a constructor. Methods add, subtract, multiply and divide are used for 
  * performing arithmetic operations between the total variable and the value
- * passed as parameter to the methods. It also contains getHistory method which
- * returns the operations performed in a string as history of calculation.
+ * passed as parameter to the methods.
  * 
  * @author Sriram Poondi Chinappa 
  * @pin 706
@@ -16,62 +15,32 @@ package cse360assign3;
 public class Calculator {
 
 	private int total;
-	private String result = "0";
-
-	/*
-	* Constructor for the Calculator class.
-	*/
+	
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
 	}
 	
-	/*
-	*  This method returns total value.
-	*/
 	public int getTotal () {
-		return total;
+		return 0;
 	}
 	
-	/*
-	*  This method adds the total with the value which is passed as a parameter.
-	*  @param value - value that needs to be added to the total.
-	*/
 	public void add (int value) {
-		result += " + " + Integer.toString(value);
-		total = total + value;
+		
 	}
 	
-	/*
-	*  This method subtracts value from total.
-	*  @param value - value that needs to be subtracted from total.
-	*/
 	public void subtract (int value) {
-		result += " - " + Integer.toString(value);
-		total = total - value;
+		
 	}
 	
-	/*
-	*  This method performs multiplication between total and value.
-	*  @param value - value that needs to be multiplied by the total.
-	*/
 	public void multiply (int value) {
-		result += " * " + Integer.toString(value);
-		total = total * value;
+		
 	}
 	
-	/*
-	*  This method performs division operation which divides the total by the parameter "value".
-	*  @param value - value that needs to be divided from the total.
-	*/
 	public void divide (int value) {
-		result += " / " + Integer.toString(value);
-		total = (value == 0) ? 0 : total / value;
+		
 	}
 	
-	/*
-	*  This method returns the history of arithmetic operations performed during calculation.
-	*/
 	public String getHistory () {
-		return result;
+		return "";
 	}
 }
--- CSE360Assignment/src/cse360assign3/CalculatorTest.java
+++ CSE360Assignment/src/cse360assign3/CalculatorTest.java
@@ -1,96 +1,49 @@
 package cse360assign3;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 /**
  * @author Sriram Poondi Chinappa 
  * @pin 706
  * @ID 1209360616
- *
  */
-
-import static org.junit.Assert.*;
-import org.junit.Test;
-
 public class CalculatorTest {
 
 	@Test
 	public void testCalculator() {
-		// Test for constructor.
 		Calculator cal = new Calculator();
 		assertNotNull(cal);
 	}
 	
 	@Test
 	public void getTotal_Test() {
-		// Test for getTotal method.
-		Calculator cal = new Calculator();
-		cal.add(5);
-		cal.subtract(2);
-		assertEquals("Total : ", 3,  cal.getTotal());
+		fail("Not yet implemented");
 	}
 	
 	@Test
 	public void add_Test() {
-		// Test for add method.
-		Calculator cal = new Calculator();
-		cal.add(5);
-		cal.add(-1);
-		assertNotEquals(6, cal.getTotal());
-		assertEquals("Total after Add() : ", 4, cal.getTotal());
+		fail("Not yet implemented");
 	}
 	
 	@Test
 	public void subtract_Test() {
-		// Test for subtract method.
-		Calculator cal = new Calculator();
-		cal.subtract(5);
-		cal.subtract(-5);
-		assertNotEquals(10, cal.getTotal());
-		assertEquals("Total after Subtract() : ", 0, cal.getTotal());
+		fail("Not yet implemented");
 	}
 	
 	@Test
 	public void multiply_Test() {
-		// Test for multiply method.
-		Calculator cal = new Calculator();
-		cal.add(5);
-		cal.multiply(6);
-		assertNotEquals(11, cal.getTotal());
-		assertEquals("Total after Multiply() : ", 30, cal.getTotal());
+		fail("Not yet implemented");
 	}
 	
 	@Test
 	public void divide_Test() {
-		// Test for divide method.
-		Calculator cal = new Calculator();
-		cal.add(5);
-		cal.divide(5);
-		assertNotEquals(0, cal.getTotal());
-		assertEquals("Total after Divide() : ", 1, cal.getTotal());
-		cal.divide(0);
-		assertEquals("Total after Divide by 0 :", 0, cal.getTotal());
+		fail("Not yet implemented");
 	}
 	
 	@Test
-	public void divideByZero_Test() {
-		// Test for special case: Divide by Zero must return total as 0.
-		Calculator cal = new Calculator();
-		cal.add(5);
-		cal.divide(0);
-		assertEquals("Total after Divide by 0 :", 0, cal.getTotal());
-	}
-
-	@Test
 	public void getHistory_Test() {
-		// Test for getHistory method.
-		Calculator cal = new Calculator();
-		cal.add(5);
-		cal.divide(5);
-		cal.subtract(1);
-		cal.divide(0);
-		cal.add(10);
-		cal.multiply(10);
-		assertNotNull("History : ", cal.getHistory());
-		assertEquals("History : ", "0 + 5 / 5 - 1 / 0 + 10 * 10", cal.getHistory());
-		assertEquals("Total : ", 100, cal.getTotal());
+		fail("Not yet implemented");
 	}
 }
