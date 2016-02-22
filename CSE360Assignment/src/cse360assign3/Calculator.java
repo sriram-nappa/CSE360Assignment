aaaa81fa24b634d033c45a96027ef9f65595d7f1
Sriram Poondi Chinappa <2k6sriram@gmail.com>
2016-02-21 21:41:14 -0700

    Assignment 3 - Initial Version

---
+++ CSE360Assignment/src/cse360assign3/Calculator.java
@@ -0,0 +1,46 @@
+package cse360assign3;
+
+/**
+ * Initial Version of calculator class contains seven stub methods including
+ * a constructor. Methods add, subtract, multiply and divide are used for 
+ * performing arithmetic operations between the total variable and the value
+ * passed as parameter to the methods.
+ * 
+ * @author Sriram Poondi Chinappa 
+ * @pin 706
+ * @ID 1209360616
+ * 
+ */
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+	
+	public void multiply (int value) {
+		
+	}
+	
+	public void divide (int value) {
+		
+	}
+	
+	public String getHistory () {
+		return "";
+	}
+}
---
+++ CSE360Assignment/src/cse360assign3/testCalculator.java
@@ -0,0 +1,53 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author Sriram Poondi Chinappa 
+ * @pin 706
+ * @ID 1209360616
+ *
+ */
+public class testCalculator {
+
+	@Test
+	public void Calculator_Test() {
+		Calculator cal = new Calculator();
+		assertNotNull(cal);
+	}
+	
+	@Test
+	public void getTotal_Test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void add_Test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void subtract_Test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void multiply_Test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void divide_Test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void getHistory_Test() {
+		fail("Not yet implemented");
+	}
+	
+	
+}
